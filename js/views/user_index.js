73d84019b9536e52f2f41df87297d3aefe379b53
Jacob <jakegibs617@gmail.com>
2015-10-29 18:50:17 -0400

    remove all button

--- js/views/user_index.js
+++ js/views/user_index.js
@@ -7,22 +7,6 @@ APP.UserIndexView = Backbone.View.extend({
     this.users = options.users;
     this.users.bind('reset', this.addAll, this);
   },
-
-  events: {
-    "click a.deleteall": "destroy"
-  },
-
-  destroy: function (event) {
-    console.log("made it here")
-
-    // event.preventDefault();
-    // event.stopPropagation();
-    // we would call
-    // this.model.destroy();
-    // which would make a DELETE call to the server with the id of the item
-    this.users.remove(this.user);
-    this.$el.remove();
-  },
   // populate the html to the dom
   render: function () {
     this.$el.html($('#indexTemplate').html());
